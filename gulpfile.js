4a46a0b3b71a3e66b8571d3fb210955ce110cb0f
Dmitriy Davydov <dvdvdmt@gmail.com>
2018-10-26 15:32:28 +0500

    Add basic automation of build process
    module6-task1

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,65 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var less = require("gulp-less");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+const { src, dest, watch, series } = require('gulp');
+const less = require('gulp-less');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const browser = require('browser-sync').create();
+const del = require('del');
 
-gulp.task("css", function () {
-  return gulp.src("source/less/style.less")
+function clean() {
+  return del(['build/']);
+}
+
+function copy() {
+  return src([
+    'source/fonts/**/*.{woff,woff2}',
+    'source/img/**',
+    'source/js/**',
+  ], {
+    base: 'source'
+  })
+    .pipe(dest('build/'));
+}
+
+function refresh(done) {
+  server.reload();
+  done();
+}
+
+function css() {
+  return src('source/less/style.less')
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
-});
+    .pipe(dest('build/css'))
+    .pipe(browser.stream());
+}
 
-gulp.task("server", function () {
-  server.init({
-    server: "source/",
+function html() {
+  return src('source/*.html')
+    // TODO add post-html task for includes
+    .pipe(dest('build/'))
+}
+
+function server() {
+  browser.init({
+    server: 'build/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
-});
+  watch('source/less/**/*.less', series(css));
+  // TODO: add task for svg-sprite building and inclusion
+  // watch('source/img/icon-*.svg', series(sprite, html, refresh));
+  watch('source/*.html', series(html, refresh));
+}
 
-gulp.task("start", gulp.series("css", "server"));
+const build = series(clean, copy, css, html);
+exports.build = build;
+exports.start = series(build, server);
